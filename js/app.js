c4f9749cfa0c576e8376d7b6ffb3008771172530
Brkcnulusy <brkcnulusy@gmail.com>
2024-04-16 16:42:57 +0300

    Feat #2 | Styling Done.

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const Todo = (function (){
+
+    // Variables
+
+
+    // Event Listeners
+
+
+
+    // FunC
+
+
+
+    return {
+        init: function () {
+
+        }
+    }
+})();
+
+document.addEventListener('DOMContentLoaded', function () {
+    Todo.init();
+});
